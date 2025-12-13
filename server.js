c2f1e803b086ae259de5820394f938ae7d23ac6a
Jack <jeyden94@gmail.com>
2025-12-13 12:28:32 -0600

    Update server.js to match node.js docs

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-const http = require('http');
+const { createServer } = require('node:http');
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from Node.js server!\n');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
 });
 
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
